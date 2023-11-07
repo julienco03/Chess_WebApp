21f1360b0b18fcb4605e8aed67817024166c3c84
julienco03 <julian160903@gmail.com>
2023-11-07 08:35:33 +0100

    Make a few adjustments

--- app/controllers/GameInteractionController.scala
+++ app/controllers/GameInteractionController.scala
@@ -11,7 +11,7 @@ class GameInteractionController @Inject()(override val controllerComponents: Con
   val controller: Controller = Controller(field = Board(), fileIO = null)
   def chessBoardAsText: String = controller.board_to_string_c()
   def chessBoardFields: Array[Array[String]] = controller.field.board.values.grouped(8).toArray.map(row => row.toArray)
-  def playerTurn: String = "TODO"
+  var playerTurn: String = "Spieler 1"
 
 
   def chess: Action[AnyContent] = Action {
@@ -20,11 +20,13 @@ class GameInteractionController @Inject()(override val controllerComponents: Con
 
   def undoMove: Action[AnyContent] = Action {
     controller.undo()
+    changePlayer
     Redirect("/chess")
   }
 
   def redoMove: Action[AnyContent] = Action {
     controller.redo()
+    changePlayer
     Redirect("/chess")
   }
 
@@ -35,6 +37,12 @@ class GameInteractionController @Inject()(override val controllerComponents: Con
       val new_pos = queryParams("new")
       controller.domove()
       controller.move_c(old_pos, new_pos)
+      changePlayer
       Redirect("/chess")
+  }
+
+  def changePlayer = {
+    if (playerTurn == "Spieler 1") playerTurn = "Spieler 2"
+    else if (playerTurn == "Spieler 2") playerTurn = "Spieler 1"
   }
 }
