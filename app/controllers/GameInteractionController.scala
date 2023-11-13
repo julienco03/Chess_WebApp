f2efc3791a7f41b2e7cab997d6e61f5c4fd324d4
julienco03 <julian160903@gmail.com>
2023-11-13 07:31:41 +0100

    Add newGame Action and update CSS

--- app/controllers/GameInteractionController.scala
+++ app/controllers/GameInteractionController.scala
@@ -2,31 +2,39 @@ package controllers
 
 import de.htwg.se.Chess.controller.controllerComponent.Controller
 import de.htwg.se.Chess.model.Board
+import de.htwg.se.Chess.model.Board
 import play.api.mvc.*
 import javax.inject.*
 
 @Singleton
 class GameInteractionController @Inject()(override val controllerComponents: ControllerComponents) extends AbstractController(controllerComponents) {
 
-  val controller: Controller = Controller(field = Board(), fileIO = null)
+  var controller: Controller = Controller(field = Board(), fileIO = null)
+  var playerTurn: String = "Spieler 1"
+  val playerNames: Array[String] = Array()  // TODO: Implement player names
   def chessBoardAsText: String = controller.board_to_string_c()
   def chessBoardFields: Array[Array[String]] = controller.field.board.values.grouped(8).toArray.map(row => row.toArray)
-  var playerTurn: String = "Spieler 1"
 
 
   def chess: Action[AnyContent] = Action {
     Ok(views.html.chess(playerTurn, chessBoardFields))
   }
 
+  def newGame: Action[AnyContent] = Action {
+    controller = Controller(field = Board(), fileIO = null)
+    playerTurn = "Spieler 1"
+    Redirect("/chess")
+  }
+
   def undoMove: Action[AnyContent] = Action {
     controller.undo()
-    changePlayer
+    changePlayer()
     Redirect("/chess")
   }
 
   def redoMove: Action[AnyContent] = Action {
     controller.redo()
-    changePlayer
+    changePlayer()
     Redirect("/chess")
   }
 
@@ -37,11 +45,11 @@ class GameInteractionController @Inject()(override val controllerComponents: Con
       val new_pos = queryParams("new")
       controller.domove()
       controller.move_c(old_pos, new_pos)
-      changePlayer
+      changePlayer()
       Redirect("/chess")
   }
 
-  def changePlayer = {
+  def changePlayer(): Unit = {
     if (playerTurn == "Spieler 1") playerTurn = "Spieler 2"
     else if (playerTurn == "Spieler 2") playerTurn = "Spieler 1"
   }
