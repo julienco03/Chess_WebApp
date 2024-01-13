51da2531aa5043aa7f5eecef498c91317629d5bb
julienco03 <julian160903@gmail.com>
2024-01-13 16:07:25 +0100

    Install vue packages for PWA

--- vue_ui/src/main.js
+++ vue_ui/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/router/router";
+import './registerServiceWorker'
 
 const app = createApp(App);
 app.use(router);
